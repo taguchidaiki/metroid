77133e2d689b796bb8f0d20999ba7c052d55a938
taguchidaiki <lost_over_21@yahoo.co.jp>
2019-12-23 09:12:56 +0900

    aaaa

--- Classes/mojule/check/CheckHitObj.cpp
+++ Classes/mojule/check/CheckHitObj.cpp
@@ -7,22 +7,28 @@ bool CheckHitObj::operator()(cocos2d::Sprite& sp, ActMojule& act)
 	auto hitScene = (cocos2d::TMXTiledMap*)director->getRunningScene()->getChildByName("hitObj")->getChildByName("map");
 	auto hitNode = hitScene->getLayer("field");
 
-	auto i = hitNode->getLayerSize().height;
-	auto vec = cocos2d::Vec2((((Obj&)sp).getPosition().x) / hitNode->getMapTileSize().width,
-		(hitNode->getLayerSize().height - ((Obj&)sp).getPosition().y - act.hitRect.y / 2) / hitNode->getMapTileSize().height);
+	//プレイヤーの真下の位置情報
+	auto hitRectData = cocos2d::Rect(((Obj&)sp).getPosition().x - act.hitRect.x / 2, ((Obj&)sp).getPosition().y + act.hitRect.y / 2, act.hitRect.x,act.hitRect.y);
 
-	int x = (((Obj&)sp).getPosition().x) / hitNode->getMapTileSize().width;
-	int y = (hitNode->getLayerSize().height - ((Obj&)sp).getPosition().y - act.hitRect.y / 2) / hitNode->getMapTileSize().height;
-	auto checkUnder = (cocos2d::TMXTiledMap*)hitNode->getTileAt(cocos2d::Vec2((((Obj&)sp).getPosition().x) / hitNode->getMapTileSize().width,
-																			  (hitNode->getMapTileSize().height - ((Obj&)sp).getPosition().y - act.hitRect.y / 2) / hitNode->getMapTileSize().height));
+	auto hitPos = cocos2d::Vec2(((Obj&)sp).getPosition().x, ((Obj&)sp).getPosition().y - act.hitRect.y / 2);
+	
+	//真下のタイル位置の情報
+	auto tileNum = cocos2d::Vec2(hitPos.x / hitNode->getMapTileSize().width,
+								 hitNode->getLayerSize().height - hitPos.y / hitNode->getMapTileSize().height);
+	auto tileData = (cocos2d::TMXTiledMap*)hitNode->getTileAt(cocos2d::Vec2((int)tileNum.x,
+																			(int)tileNum.y));
 
-	if (checkUnder != nullptr)
+	if (tileData != nullptr)
 	{
-		auto pos = checkUnder->getPosition().y + hitNode->getMapTileSize().height / 2;
-		((Obj&)sp).setPositionY(checkUnder->getPosition().y + hitNode->getMapTileSize().height / 2);
+		((Obj&)sp).setPositionY(act.hitRect.y / 2 + tileData->getPosition().y + hitNode->getMapTileSize().height);
+		((Obj&)sp).stateName("player-idle-r");
+	}
+	else
+	{
+		//((Obj&)sp).stateName("player-fall-r");
 	}
 
-	if (((Obj&)sp).getPosition().y - act.hitRect.y / 2 <= 0)
+	/*if (((Obj&)sp).getPosition().y - act.hitRect.y / 2 <= 0)
 	{
 		((Obj&)sp).setPosition(cocos2d::Vec2(((Obj&)sp).getPosition().x, act.hitRect.y / 2));
 		if (((Obj&)sp).stateName() == "player-jump-l")
@@ -33,7 +39,7 @@ bool CheckHitObj::operator()(cocos2d::Sprite& sp, ActMojule& act)
 		{
 			((Obj&)sp).stateName("player-idle-r");
 		}
-	}
+	}*/
 
 	return true;
 }
